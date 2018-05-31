79d73e9a82ca2b696b19385f18146f3a6594f6c3
Chang Sophia <you@example.com>
2018-05-31 12:04:27 +0800

    add win-lose

--- Demo/seedweedplayViewController.swift
+++ Demo/seedweedplayViewController.swift
@@ -12,9 +12,13 @@ class seedweedplayViewController: UIViewController {
     
    
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var winLabel: UILabel!
+    @IBOutlet weak var loseLabel: UILabel!
     @IBOutlet weak var princessImageView: UIImageView!
     
     let princesses = ["cinderella", "ariel", "belle","elsa", "anna", "aurora"]
+    var win: Int = 0
+    var lose: Int = 0
     
     @IBOutlet weak var arielButton: UIButton!
     @IBOutlet weak var annaButton: UIButton!
@@ -22,9 +26,9 @@ class seedweedplayViewController: UIViewController {
     @IBOutlet weak var cinderellaButton: UIButton!
     @IBOutlet weak var auroraButton: UIButton!
     @IBOutlet weak var belleButton: UIButton!
-    
-
-    
+    @IBOutlet weak var gradeView: UIImageView!
+    @IBOutlet weak var gradeLabel: UILabel!
+   
     
     func transparentButtons() {
         cinderellaButton.alpha = 0.2
@@ -38,8 +42,6 @@ class seedweedplayViewController: UIViewController {
     
    
     @IBAction func play(_ sender: UIButton) {
-     
-        
         let randomDistribution = GKRandomDistribution(lowestValue: 0, highestValue: princesses.count - 1)
         let number = randomDistribution.nextInt()
         let princess = princesses[number]
@@ -47,70 +49,64 @@ class seedweedplayViewController: UIViewController {
         
     transparentButtons()
         var isWin = false
-        
-        
         if sender ==  arielButton {
             arielButton.alpha = 1
-           
             if princess == "ariel" {
                 isWin = true
             }
         } else if sender == cinderellaButton {
             cinderellaButton.alpha = 1
-            
             if princess == "cinderella" {
                 isWin = true
             }
         } else if sender == belleButton {
             belleButton.alpha = 1
-           
             if princess == "belle" {
                 isWin = true
             }
         }   else if sender == elsaButton {
                     elsaButton.alpha = 1
-                    
                     if princess == "elsa" {
                         isWin = true
             }
             
         }   else if sender == auroraButton {
             auroraButton.alpha = 1
-            
             if princess == "aurora" {
                 isWin = true
             }
             
         }   else if sender == annaButton {
             annaButton.alpha = 1
-            
             if princess == "anna" {
                 isWin = true
             
             }
         
         }
-        
+      
         if isWin {
             resultLabel.text = "Win"
+            win = win + 1
+            winLabel.text = "勝：\(win)"
     
         } else {
             resultLabel.text = "Lose"
+            lose = lose + 1
+            loseLabel.text = "敗：\(lose)"
         }
     }
     
-    
-   override  func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-   override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
